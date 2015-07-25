f8592032e461536589334e6b7283e51ec57415bf
Kirk Elliott <kirk@addthis.com>
2015-07-25 13:49:47 -0400

    deleted files

--- platforms/dandroid/assets/www/js/app/ui/responsive.js
+++
@@ -1,9 +0,0 @@
-module.exports = (function () {
-  var win = $(window)
-    , w = win.width()
-    , h = win.height();
-
-  if (parseInt(Math.min(w, h), 10) >= 600) {
-	  $('body').addClass('tablet');
-  }
-}());
