7bcee0809655cad81a0ede7269b3f83c75d893ed
Kirk Elliott <kirk@addthis.com>
2015-07-25 13:00:25 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# carnegie-ru
+phonegap-feed-reader
+====================
+
+To use:
+make sure XCode is installed
+make sure cordova is installed, I recommend 3.4.1 or greater
+npm install cordova
+
+where folderName is the name of the new folder to create
+git clone git@github.com:dmvjs/phonegap-feed-reader.git folderName
+
+enter the directory (change folderName to folder in step #1 if you changed it)
+cd folderName
+
+cordova platform add ios
+
+cordova build ios
+
+by now you should be able to open the platforms/ios/Carnegie xcodeproj file in XCode and (Command-R) run the application
+
+if you make changes to files within www folder, cd to www/js and run these commands:
+
+browserify index.js -o app.js
+
+cordova build ios
+
+
+
+
---
+++ www/js/app/config.js
@@ -0,0 +1,85 @@
+/*global module, require*/
+
+module.exports = {
+    fs: void 0
+    , appName: 'Carnegie.ru'
+    , track: true
+    , trackId: 'UA-31877-32'
+    , folder: 'com.ceip.carnegieru'
+    , storyFontSize: 1.0
+    , connectionMessage: 'No network connection detected'
+    , menuMessage: 'Not yet downloaded'
+    , menuMessageRu: 'Пока не установлено'
+    , missingImage: 'http://carnegieendowment.org/app-img-not-avail.png'
+    , missingImageRef: void 0
+    , menu: [{
+        title: 'Now from Carnegie.ru'
+        , sub: 'Read Offline'
+        , feeds: [{
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu'
+            , name: 'Latest Analysis'
+            , filename: 'carnegieru-latest.json'
+            , type: 'json'
+            , required: true
+        }, {
+            url: 'http://carnegieendowment.org/rss/feeds/mobile-carnegie-moscow-top5.json.txt'
+            , name: 'Most Popular'
+            , filename: 'carnegieru-popular.json'
+            , type: 'json'
+        }, {
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu&channel=short'
+            , name: 'Commentary'
+            , filename: 'carnegieru-commentary.json'
+            , type: 'json'
+        }, {
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu&channel=long'
+            , name: 'Research'
+            , filename: 'carnegieru-research.json'
+            , type: 'json'
+        }]
+    }, {
+        title: 'Сегодня на Carnegie.ru'
+        , sub: 'Скачать'
+        , v: 'R'
+        , feeds: [{
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu&lang=ru'
+            , name: 'Недавние публикации'
+            , filename: 'carnegieru-latest-ru.json'
+            , type: 'json'
+        }, {
+         url: 'http://carnegieendowment.org/rss/feeds/mobile-carnegie-moscow-russian-top5.json.txt'
+         , name: 'Самое популярное)'
+         , filename: 'carnegieru-popular-ru.json'
+         , type: 'json'
+         }, {
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu&channel=short&lang=ru'
+            , name: 'Статьи и интервью'
+            , filename: 'carnegieru-commentary-ru.json'
+            , type: 'json'
+        }, {
+            url: 'http://carnegieendowment.org/rss/solr/?fa=AppCarnegieRu&channel=long&lang=ru'
+            , name: 'Исследования'
+            , filename: 'carnegieru-research-ru.json'
+            , type: 'json'
+        }]
+    }, {
+        title: 'Browse Topics'
+        , links: [{
+            url: 'http://carnegie.ru/about/?lang=en'
+            , name: 'About Carnegie Moscow Center'
+        }, {
+            url: 'http://carnegie.ru/experts/?lang=en'
+            , name: 'About our Experts'
+        }]
+    }, {
+        title: 'Просмотр темы'
+        , v: 'R'
+        , links: [{
+            url: ' http://carnegie.ru/about/'
+            , name: 'О Московском Центре Карнеги'
+        }, {
+            url: 'http://carnegie.ru/experts/'
+            , name: 'Эксперты Московского Центра'
+        }]
+    }]
+};
